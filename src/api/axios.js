6181d834d0641c2a798cbab966b0a50c594f2683
shubham9803 <shubham.a.bhor@gmail.com>
2026-03-09 00:59:55 +0530

    Added backend URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://127.0.0.1:8000/";
+const baseURL = "https://web-production-1a2f8.up.railway.app/";
 
 const axiosInstance = axios.create({
   baseURL,
